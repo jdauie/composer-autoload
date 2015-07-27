0380f961ec040bde42cccd37f729479e89b34349
Joshua Morey <josh@joshmorey.com>
2015-07-26 22:11:30 -0600

    Added plugin

---
+++ ComposerAutoloadPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jacere;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PostAutoloadDumpEvent;
+
+class ComposerAutoloadPlugin implements PluginInterface, EventSubscriberInterface {
+
+	protected $composer;
+	protected $io;
+
+	public function activate(Composer $composer, IOInterface $io) {
+		$this->composer = $composer;
+		$this->io = $io;
+	}
+
+	public static function getSubscribedEvents() {
+		return [
+			PluginEvents::POST_AUTOLOAD_DUMP => [
+				['onPostAutoloadDump', 0],
+			],
+		];
+	}
+
+	public function onPostAutoloadDump(PostAutoloadDumpEvent $event)
+	{
+		//
+	}
+}
